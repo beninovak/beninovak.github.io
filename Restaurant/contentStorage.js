41760593e04437ebce8366ef4d57a5ba2a105f73
beninovak <beni.novak13@gmail.com>
2022-02-08 10:04:30 +0100

    Cleaned up files
    Removed partial refresh as it was causing graphical glitches on refresh by removing some .js files. Removed unused image. Index html now links to subpages -> temporary solution.

--- Restaurant/contentStorage.js
+++
@@ -1,5 +0,0 @@
-// If you visited any subpage in this browser session
-// it remembers which and loads it when you reload the site
-if(sessionStorage.getItem("lastOpenedSubpage") != null) {
-    mainContentFetch(sessionStorage.getItem("lastOpenedSubpage"));
-}
