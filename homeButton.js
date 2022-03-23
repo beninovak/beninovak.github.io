301552007d24fa2cec32e2d241c297e84de3e33a
beninovak <beni.novak13@gmail.com>
2022-03-23 09:55:49 +0100

    Updated "home button" background color

--- homeButton.js
+++ homeButton.js
@@ -1,8 +1,8 @@
 const homeButton = document.createElement("a");
 const icon = document.createElement("img");
 
-// Style declared to avoid having to link a seperate css file to every .html file
-// and to have a single declaration of the style that will propagate to all pages if it is changed
+// Style declared to avoid having to link a seperate css file-
+// to every .html file and to have a single declaration of the style.
 const style = `
     width: 50px; 
     height: 50px; 
@@ -13,20 +13,20 @@ const style = `
     text-align: center; 
     border: 3px solid white; 
     outline: 3px solid black; 
-    background-color: #c9823a; 
+    background-color: #7366c7;
     cursor: pointer;
     z-index: 1000;
     box-sizing: content-box;`;
 
 const iconStyle = "width: 35px; height: 45px;"
 
-// Styles the button and links it to the homepage
-homeButton.style.cssText = style;
-homeButton.href = "../index.html";
-
 // Styles the icon
 icon.style.cssText = iconStyle;
 icon.src = "../house.svg";
+
+// Styles the button and links it to the homepage
+homeButton.style.cssText = style;
+homeButton.href = "../index.html";
 
 homeButton.appendChild(icon);
 document.body.appendChild(homeButton);
